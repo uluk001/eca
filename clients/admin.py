8c7d6784ff52bd69ea17efb153be4252fc5158e1
Hacker <Hacker@MacBook-Pro-2.local>
2023-04-02 16:53:07 +0600

    nothing

--- clients/admin.py
+++ clients/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, OurClients
+from .models import Category, OurClients, PartnerImage
 
 
 admin.site.register(Category)
+admin.site.register(PartnerImage)
 admin.site.register(OurClients)
