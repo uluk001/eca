69690e1260df72cb3a711e26c35b7e27cb63f2f6
Hacker <Hacker@MacBook-Pro-2.local>
2023-04-04 16:18:45 +0600

    added endpoint

--- associated_aca_memebers/urls.py
+++ associated_aca_memebers/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from .views import MembersView
+from django.urls import path, re_path
+from .views import MembersView, EcasRoleList
 
 
 urlpatterns = [
     path('list/', MembersView.as_view()),
+    re_path('role/(?P<id>.+)/$', EcasRoleList.as_view()),
 ]
--- associated_aca_memebers/views.py
+++ associated_aca_memebers/views.py
@@ -1,5 +1,5 @@
-from .serializers import MembersSerializer
-from .models import Members
+from .serializers import MembersSerializer, EcasRoleSerializer
+from .models import Members, EcasRole
 from rest_framework.generics import ListAPIView
 from .paginators import CustomPagination
 
@@ -7,3 +7,10 @@ class MembersView(ListAPIView):
     serializer_class = MembersSerializer
     queryset = Members.objects.all()
     pagination_class = CustomPagination
+
+class EcasRoleList(ListAPIView):
+    serializer_class = EcasRoleSerializer
+
+    def get_queryset(self):
+        member = Members.objects.get(id=self.kwargs['id'])
+        return EcasRole.objects.filter(member=member)
