41ed038f7c348953f70b670289a12814aaea425f
Hacker <Hacker@MacBook-Pro-2.local>
2023-04-06 00:27:11 +0600

    fields in resume made optional

--- forms/models.py
+++ forms/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    full_name = models.CharField(max_length=255, verbose_name='Имя')
-    email = models.EmailField(verbose_name='Электронная почта')
-    phone_number = models.CharField(max_length=15, verbose_name='Номер телефона')
+    full_name = models.CharField(max_length=255, verbose_name='Имя', null=True, blank=True)
+    email = models.EmailField(verbose_name='Электронная почта', null=True, blank=True)
+    phone_number = models.CharField(max_length=15, verbose_name='Номер телефона', null=True, blank=True)
 
 
 class Resume(models.Model):
     first_name = models.CharField(max_length=255, verbose_name='Имя')
     last_name = models.CharField(max_length=255, verbose_name='Фамилия')
-    email = models.EmailField(verbose_name='Электронная почта')
+    email = models.EmailField(verbose_name='Электронная почта', null=True, blank=True)
     resume = models.FileField(upload_to='resume')
